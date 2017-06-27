bcbd6314ee6b08159539cf685b16007414fdd372
Hung Lay <hunglay@Hungs-MacBook-Air.local>
2017-06-26 22:00:29 -0700

    adding stylesheet.css

---
+++ imports/startup/client/index.js
@@ -0,0 +1,8 @@
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+import routes from './routes.jsx';
+import '/imports/ui/stylesheet/stylesheet.css';
+
+Meteor.startup(() => {
+  render(routes(), document.getElementById('app'));
+});
